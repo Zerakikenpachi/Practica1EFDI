6d12c02de839eb3c70206fc9d288042e78a21b90
VictorRpo <victtestrepo@gmail.com>
2017-09-02 22:41:38 -0500

    Esqueleto Java Application

---
+++ Practica1EFDI/src/com/business/Business.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.business;
+
+/**
+ *
+ * @author victor restrepo
+ */
+public class Business {
+    public void main (String[] args){
+        System.out.println("Felicidades tu clase Bussiness esta funcionando XD");
+    }
+}
