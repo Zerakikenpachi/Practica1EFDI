3627403bdaff10f4b8aa1524341f273393be1844
VictorRpo <victtestrepo@gmail.com>
2017-09-08 11:30:19 -0500

    Actualización de las clases venta y cliente

---
+++ Practica1EFDI-Victor/Practica1EFDI/src/com/model/Persona.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.model;
+
+/**
+ *
+ * @author victor restrepo
+ */
+public class Persona {
+
+    private String identificacion;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String numeroCelular;
+    private String genero;
+
+    public Persona(String identificacion, String nombre, String apellido, String correo, String numeroCelular, String genero) {
+        this.identificacion = identificacion;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.numeroCelular = numeroCelular;
+        this.genero = genero;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNumeroCelular() {
+        return numeroCelular;
+    }
+
+    public void setNumeroCelular(String numeroCelular) {
+        this.numeroCelular = numeroCelular;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    
+
+}
---
+++ Practica1EFDI-Victor/Practica1EFDI/src/com/model/Venta.java
@@ -0,0 +1,151 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.model;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author victor restrepo
+ */
+public class Venta {
+
+    private static final long serialVersionUID = 1L;
+    private String idVenta;
+    private int valorProducto;
+    private int descuento;
+    private int valorNeto;
+    private Date fecha;
+    private Date hora;
+    private String medioPago;
+    private String idAlmacen;
+    private String idCajero;
+    private String idCliente;
+
+    public Venta(String idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public Venta(String idVenta, int valorProducto, int descuento, int valorNeto, Date fecha, Date hora, String medioPago, String idAlmacen, String idCajero, String idCliente) {
+        this.idVenta = idVenta;
+        this.valorProducto = valorProducto;
+        this.descuento = descuento;
+        this.valorNeto = valorNeto;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.medioPago = medioPago;
+        this.idAlmacen = idAlmacen;
+        this.idCajero = idCajero;
+        this.idCliente = idCliente;
+    }
+
+    public String getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(String idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public int getValorProducto() {
+        return valorProducto;
+    }
+
+    public void setValorProducto(int valorProducto) {
+        this.valorProducto = valorProducto;
+    }
+
+    public int getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(int descuento) {
+        this.descuento = descuento;
+    }
+
+    public int getValorNeto() {
+        return valorNeto;
+    }
+
+    public void setValorNeto(int valorNeto) {
+        this.valorNeto = valorNeto;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Date getHora() {
+        return hora;
+    }
+
+    public void setHora(Date hora) {
+        this.hora = hora;
+    }
+
+    public String getMedioPago() {
+        return medioPago;
+    }
+
+    public void setMedioPago(String medioPago) {
+        this.medioPago = medioPago;
+    }
+
+    public String getIdAlmacen() {
+        return idAlmacen;
+    }
+
+    public void setIdAlmacen(String idAlmacen) {
+        this.idAlmacen = idAlmacen;
+    }
+
+    public String getIdCajero() {
+        return idCajero;
+    }
+
+    public void setIdCajero(String idCajero) {
+        this.idCajero = idCajero;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idVenta != null ? idVenta.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Venta)) {
+            return false;
+        }
+        Venta other = (Venta) object;
+        if ((this.idVenta == null && other.idVenta != null) || (this.idVenta != null && !this.idVenta.equals(other.idVenta))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.model.Venta[ idVenta=" + idVenta + " ]";
+    }
+
+}
